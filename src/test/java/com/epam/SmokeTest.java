c3349e61dee6ce96d86e458f49a2ae541017ae38
Shaheen Firdous <shaheen_firdous@epam.com>
2022-07-21 16:15:50 +0530

    Initial commit

---
+++ src/test/java/com/epam/SmokeTest.java
@@ -0,0 +1,11 @@
+package com.epam;
+import org.junit.jupiter.api.Test;
+
+public class SmokeTest {
+    @Test
+    public void test(){
+        System.out.println("hello this is my first testcase");
+
+    }
+
+}
